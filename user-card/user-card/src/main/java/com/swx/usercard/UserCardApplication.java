8d92a60fb574ed7354889484c354709dc6f4c3ba
songweixiong <513896929@qq.com>
2019-07-25 20:50:34 +0800

    上传user-card工程

---
+++ user-card/user-card/src/main/java/com/swx/usercard/UserCardApplication.java
@@ -0,0 +1,13 @@
+package com.swx.usercard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserCardApplication.class, args);
+	}
+
+}
