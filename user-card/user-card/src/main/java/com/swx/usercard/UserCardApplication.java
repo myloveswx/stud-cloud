47b681c7e251c0f75b01740d69ddd7500525d867
songweixiong <513896929@qq.com>
2019-07-25 21:01:34 +0800

    增加注释

--- user-card/user-card/src/main/java/com/swx/usercard/UserCardApplication.java
+++ user-card/user-card/src/main/java/com/swx/usercard/UserCardApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UserCardApplication {
 
 	public static void main(String[] args) {
-		//启动
+		//启动1
 		SpringApplication.run(UserCardApplication.class, args);
 	}
 
